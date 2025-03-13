22209f1ec32c609d08b6e9649df1b498896fcd76
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-13 14:02:52 +0900

    페이지 생성

--- Zustand/TS/src/main.tsx
+++ Zustand/TS/src/main.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import CountPage from "./pages/countPage.tsx";
+import LoginPage from "./pages/member/loginPage.tsx";
+import ListPage from "./pages/todo/listPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
      <BrowserRouter>
         <Routes>
             <Route path={'/'} element={<App/>}></Route>
             <Route path={'/count'} element={<CountPage/>}></Route>
+            <Route path={'/member/login'} element={<LoginPage/>}></Route>
+            <Route path={'/todo/list'} element={<ListPage/>}></Route>
         </Routes>
      </BrowserRouter>
 )
---
+++ Zustand/TS/src/pages/todo/listPage.tsx
@@ -0,0 +1,13 @@
+import TopMenuComponent from "../../components/menu/topMenuComponent.tsx";
+
+function ListPage() {
+    return (
+
+        <div>
+            <TopMenuComponent/>
+            <div>Todo List Page</div>
+        </div>
+    );
+}
+
+export default ListPage;
