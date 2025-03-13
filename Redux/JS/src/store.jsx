d4006fda20d338b46c081db0bd02cdb47f1be7da
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-13 09:41:22 +0900

    비동기 호출 연습(실제로는 이렇게 하지마라)

--- Redux/JS/src/store.jsx
+++ Redux/JS/src/store.jsx
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import countSlice from "./slices/countSlice.jsx";
 import loginSlice from "./slices/loginSlice.jsx";
+import productsSlice from "./slices/productsSlice.jsx";
 
 
 const store = configureStore({
     reducer: {
         counter: countSlice,
-        login: loginSlice
+        login: loginSlice,
+        products: productsSlice
     }
 })
 
