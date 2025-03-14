873ba6d141484c89052ba20dc43e80f33e7767fa
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-14 09:26:01 +0900

    main에 Qurey추가

--- React-Query/TS/src/main.tsx
+++ React-Query/TS/src/main.tsx
@@ -7,9 +7,15 @@ import CountPage from "./pages/countPage.tsx";
 import LoginPage from "./pages/member/loginPage.tsx";
 import ListPage from "./pages/todo/listPage.tsx";
 import KioskPage from "./pages/kioskPage.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root')!).render(
-     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
         <Routes>
             <Route path={'/'} element={<App/>}></Route>
             <Route path={'/count'} element={<CountPage/>}></Route>
@@ -18,4 +24,7 @@ createRoot(document.getElementById('root')!).render(
             <Route path={'/kiosk'} element={<KioskPage/>}></Route>
         </Routes>
      </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+
 )
