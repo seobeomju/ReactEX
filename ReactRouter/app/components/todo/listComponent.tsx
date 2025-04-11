4c9180b29ab039f29ee0285fc07a02d2516e5ca8
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-11 15:40:03 +0900

    listComponent

--- ReactRouter/app/components/todo/listComponent.tsx
+++ ReactRouter/app/components/todo/listComponent.tsx
@@ -1,10 +1,26 @@
+import {useQuery} from "@tanstack/react-query";
 
 
 function TodoListComponent () {
+
+    // Queries
+    const query = useQuery({
+        queryKey: ['todos'],
+        queryFn: async() => {
+
+            //2초 딜레이
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            return {msg: "Hello World"}
+        }
+    })
+
+    const {isFetching, data, error } = query
+
     return (
         <div>
             <div className={'text-4xl'}>Todo List Component </div>
-
+            <div className={'text-3xl bg-amber-600'}> {isFetching && <h1>Loading.........</h1>}</div>
         </div>
     )
 }
