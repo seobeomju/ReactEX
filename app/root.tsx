37cfe2a38f5892e34ca22a21addb094ed97e1bab
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-11 11:43:38 +0900

    Zustand setting

--- app/root.tsx
+++ app/root.tsx
@@ -6,24 +6,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
-
-
-import type { Route } from "./+types/root";
-
-
-import "./app.css";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { Route } from "./+types/root";
+import "./app.css";
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
@@ -32,7 +25,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+      <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -40,22 +33,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      {children}
+      <ScrollRestoration />
+      <Scripts />
       </body>
-    </html>
+      </html>
   );
 }
 
-// QueryClient는 앱 수준에서 한 번만 생성해야 함
-const queryClient = new QueryClient();
-
 export default function App() {
-  <QueryClientProvider client={queryClient}>
-    <Outlet />
-    <ReactQueryDevtools initialIsOpen={true} />
-  </QueryClientProvider>
+  return (
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
@@ -66,23 +58,23 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? "404" : "Error";
     details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
+        error.status === 404
+            ? "The requested page could not be found."
+            : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+      <main className="pt-16 p-4 container mx-auto">
+        <h1>{message}</h1>
+        <p>{details}</p>
+        {stack && (
+            <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
         </pre>
-      )}
-    </main>
+        )}
+      </main>
   );
 }
--- app/routes/todo/listPage.tsx
+++ app/routes/todo/listPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import TodoListComponent from '~/components/todo/listComponent';
+import { useStore } from '~/store/useCountStore';
  
  function TodoListPage() {
+
+    const { count, inc } = useStore()
+
+
      return (
          <div>
              <div className={'text-4xl'}>Todo List Page</div>
+
+             <span>{count}</span>
+             <button onClick={inc}>one up</button>
+             <TodoListComponent></TodoListComponent>
          </div>
      );
  }
