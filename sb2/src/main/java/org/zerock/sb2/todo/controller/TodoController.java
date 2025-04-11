0b474e4b70be48e60075263ce0a0b6550e550059
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-11 14:32:21 +0900

    TodoController Post

--- sb2/src/main/java/org/zerock/sb2/todo/controller/TodoController.java
+++ sb2/src/main/java/org/zerock/sb2/todo/controller/TodoController.java
@@ -1,11 +1,14 @@
 package org.zerock.sb2.todo.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.sb2.todo.dto.ActionResultDTO;
 import org.zerock.sb2.todo.dto.TodoDTO;
 
 import java.util.List;
@@ -18,21 +21,40 @@ public class TodoController {
 
     @GetMapping("list")
     public ResponseEntity<List<TodoDTO>> list1(){
+
         List<TodoDTO> list
-                =List.of(
-                        TodoDTO.builder()
-                                .tno(10L)
-                                .title("title10")
-                                .writer("writer10")
-                                .build(),
-                        TodoDTO.builder()
-                                .tno(10L)
-                                .title("title10")
-                                .writer("writer10")
-                                .build()
+                = List.of(
+                TodoDTO.builder()
+                        .tno(10L)
+                        .title("title10")
+                        .writer("writer10")
+                        .build(),
+                TodoDTO.builder()
+                        .tno(9L)
+                        .title("title9")
+                        .writer("writer9")
+                        .build()
         );
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         return ResponseEntity.ok(list);
     }
 
+    @PostMapping("")
+    public ResponseEntity<ActionResultDTO<Long>> post(TodoDTO dto){
+
+        log.info("----------------post--------------------");
+        log.info(dto);
+
+        return ResponseEntity.ok(ActionResultDTO.<Long>builder()
+                .result("success")
+                .data(10L)
+                .build());
+
+    }
 }
---
+++ sb2/src/main/java/org/zerock/sb2/todo/dto/ActionResultDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.sb2.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActionResultDTO<T> {
+
+    private String result;
+
+    private T data;
+}
