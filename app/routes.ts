16b94e1926f2f329fad60eb9b33c33681a2bb7b9
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-11 11:03:16 +0900

    todo list config

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route('/about','routes/about.tsx')
+    route('/about','routes/about.tsx'),
+    route("/todo", "layout/todoLayout.tsx", [
+        route("list",'routes/todo/listPage.tsx')
+    ])
 ] satisfies RouteConfig;
---
+++ app/routes/todo/listPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+ function TodoListPage() {
+     return (
+         <div>
+             <div className={'text-4xl'}>Todo List Page</div>
+         </div>
+     );
+ }
+ 
+ export default TodoListPage;
