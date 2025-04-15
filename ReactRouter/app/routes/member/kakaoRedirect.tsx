c8ce9f250991bf7202ee5c320ed7d46daff9bde8
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-15 15:39:55 +0900

    kakaoLogin6

--- ReactRouter/app/routes/member/kakaoRedirect.tsx
+++ ReactRouter/app/routes/member/kakaoRedirect.tsx
@@ -1,34 +1,43 @@
-import { log } from 'console';
-import React, { useEffect } from 'react';
-import { data, useSearchParams } from 'react-router';
-import { getAccessToken } from '~/api/kakaoAPI';
- 
- function KakaoRedirect() {
+import React, {useEffect} from 'react';
+import {useSearchParams} from "react-router";
+import {getAccessToken, getMemberWithAccessToken} from "~/api/kakaoAPI";
 
-    const [searchParms] = useSearchParams()
-    //code = afsafsdvef
-    const authCode = searchParms.get('code')
+function KakaoRedirect() {
 
-    useEffect(()=>{
+    const [searchParams] = useSearchParams()
+
+    //code=xsdfsefsef
+    const authCode = searchParams.get('code')
+
+    useEffect(() => {
 
         if(authCode){
-            getAccessToken(authCode).then((data) => {
-                console.log(data)
-            });
+            getAccessToken(authCode).then(data => {
+                const accessToken = data
+                console.log("accessToken" ,accessToken)
+
+                getMemberWithAccessToken(accessToken).then(loginResult => {
+
+                    console.log(loginResult)
+
+                })
+
+            })
         }
+
     },[authCode])
 
 
-    if(!authCode){
+    if(!authCode) {
         return (<div>로그인 실패</div>)
     }
 
-     return (
-         <div>
-             <div>이 페이지는 카카오에서 호출해 주는 페이지 입니다</div>
-             <div>{authCode}</div>
-         </div>
-     );
- }
- 
- export default KakaoRedirect;
+    return (
+        <div>
+            <div>이 페이지는 카카오에서 호출해 주는 페이지 입니다</div>
+            <div>{authCode}</div>
+        </div>
+    );
+}
+
+export default KakaoRedirect;
--- sb2/src/main/java/org/zerock/sb2/member/controller/SocialController.java
+++ sb2/src/main/java/org/zerock/sb2/member/controller/SocialController.java
@@ -3,6 +3,7 @@ package org.zerock.sb2.member.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
