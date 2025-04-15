89d205dacbc0420326c9743e5212ee0157e07925
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-15 14:18:19 +0900

    kakaoLogin3

--- ReactRouter/app/routes/member/kakaoRedirect.tsx
+++ ReactRouter/app/routes/member/kakaoRedirect.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSearchParams } from 'react-router';
  
  function KakaoRedirect() {
+
+    const [searchParms] = useSearchParams()
+    //code = afsafsdvef
+    const authCode = searchParms.get('code')
+
+    if(!authCode){
+        return (<div>로그인 실패패</div>)
+    }
+
      return (
          <div>
              <div>이 페이지는 카카오에서 호출해 주는 페이지 입니다</div>
+             <div>{authCode}</div>
          </div>
      );
  }
