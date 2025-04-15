bf99d2ac0405216625f92bb3eab871017cd9451a
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-15 15:54:19 +0900

    kakaoLogin8

--- sb2/src/main/java/org/zerock/sb2/member/controller/SocialController.java
+++ sb2/src/main/java/org/zerock/sb2/member/controller/SocialController.java
@@ -6,14 +6,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.sb2.member.service.MemberService;
 
 @RestController
 @Log4j2
 @RequiredArgsConstructor
 public class SocialController {
+
+    private final MemberService memberService;
+
     @RequestMapping("/api/v1/member/kakao")
     public ResponseEntity<String[]> getkakao(String accessToken){
         log.info("getkako: " + accessToken);
+
+        String kakaoEmail = memberService.getKakaoEmail(accessToken);
+
+        log.info("kakaoEmail: " + kakaoEmail);
 
         String[] result = new String[]{"acccess......", "refresh...."};
 
