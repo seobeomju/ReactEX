6559fc66540399e064f54858d93a8a66c5146418
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-15 09:38:45 +0900

    Page, Size

--- ReactRouter/app/api/todoAPI.ts
+++ ReactRouter/app/api/todoAPI.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const host = "http://localhost:8080/api/v1/todos";
 
-export async function testTodoList() {
+export async function testTodoList(page:string, size:string ) {
 
-    const res = await axios.get(`${host}/list`)
+    const res = await axios.get(`${host}/list?page=${page}&size=${size}`);
 
     return res.data
 
--- ReactRouter/app/components/todo/listComponent.tsx
+++ ReactRouter/app/components/todo/listComponent.tsx
@@ -1,21 +1,21 @@
 import {useQuery} from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
 import {testTodoList} from "~/api/todoAPI";
 
 
 function TodoListComponent () {
 
+    const[searchParms] = useSearchParams();
+
+    const pageStr = searchParms.get("page") || "1"
+    const sizeStr = searchParms.get("size") || "10"
+
+    console.log("PageStr:" + pageStr, "sizeStr: "+ sizeStr);
+
     // Queries
     const query = useQuery({
         queryKey: ['todos'],
-        queryFn: async() => {
-
-
-            await new Promise(resolve => setTimeout(resolve, 2000));
-
-            const todos:Todo[] = await testTodoList()
-
-            return todos
-        }
+        queryFn: () => testTodoList(pageStr,sizeStr)
     })
 
     const {isFetching, data, error } = query
