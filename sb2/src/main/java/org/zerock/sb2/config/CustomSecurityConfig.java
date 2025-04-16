9d0f43ad72d8d8d68a24b665767610a689022b55
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-16 11:31:00 +0900

    JWT4

--- sb2/src/main/java/org/zerock/sb2/config/CustomSecurityConfig.java
+++ sb2/src/main/java/org/zerock/sb2/config/CustomSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.zerock.sb2.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerock.sb2.member.filter.JWTCheckFilter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +20,10 @@ import java.util.List;
 @Log4j2
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    private final JWTCheckFilter jwtCheckFilter;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -41,6 +47,8 @@ public class CustomSecurityConfig {
         http.sessionManagement(config -> {
             config.sessionCreationPolicy(SessionCreationPolicy.NEVER);
         });
+
+        http.addFilterBefore(jwtCheckFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ sb2/src/main/java/org/zerock/sb2/member/filter/JWTCheckFilter.java
@@ -0,0 +1,56 @@
+package org.zerock.sb2.member.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.sb2.util.JWTUtil;
+
+import java.io.IOException;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class JWTCheckFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+
+        log.info("-----------shouldNotFilter----------");
+        //부정의 부정 -> 긍정
+        return false;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("-----------doFilterInternal----------");
+
+        log.info("requestURI: " + request.getRequestURI());
+
+        String headerStr = request.getHeader("Authorization");
+
+        log.info("headerStr: " + headerStr);
+
+        //Access Token이 없는 경우
+        if (headerStr == null || !headerStr.startsWith("Bearer ")) {
+            handleException(response, new Exception("ACCESSS TOKEN NOT FOUND"));
+            return;
+        }
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.getWriter().println("{\"error\": \"" + e.getMessage() + "\"}");
+    }
+
+}
