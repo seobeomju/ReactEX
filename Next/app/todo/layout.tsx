8df49e9bb34d71a02653ce531705271801ca8812
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-21 10:07:27 +0900

    TodoLayout

--- Next/app/todo/layout.tsx
+++ Next/app/todo/layout.tsx
@@ -1,13 +1,20 @@
+import React from 'react';
+import Link from "next/link";
 
-function TodoLayout({children,}: Readonly<{children: React.ReactNode;}>){
-    return(
-        <div>
-            <div>Todo Layout</div>
-            <div>
-                {children}
-            </div>
+
+export default function TodoLayout({ children }: { children: React.ReactNode }) {
+
+    return (
+        <div className="p-6">
+            <nav className="mb-4 flex gap-4 border-b pb-2">
+                <Link href="/todo/list" className="text-blue-600 hover:underline">
+                    📋 Todo List
+                </Link>
+                <Link href="/todo/add" className="text-blue-600 hover:underline">
+                    ➕ Add Todo
+                </Link>
+            </nav>
+            <main>{children}</main>
         </div>
-    )
+    );
 }
-
-export default TodoLayout
