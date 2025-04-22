1d748de1a82d1da0cc5bfae6e1721c138b8c623d
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-04-22 09:24:43 +0900

    page

--- Next/app/todo/list/page.tsx
+++ Next/app/todo/list/page.tsx
@@ -1,10 +1,31 @@
 
-function TodoListPage(){
+
+import prisma from "@/lib/prisma";
+
+export const dynamic = 'force-dynamic';
+
+async function TodoListPage({searchParams}: {searchParams:Promise<any>} ) {
+
+    const search = await searchParams
+
+    console.log(search)
+    const page = search?.page || "1"
+    const size = search?.page || "10"
+
+    const data = await prisma.tbl_todo.findMany({
+        skip:(Number(page)-1) * Number(size),
+        take: Number(size),
+        orderBy: {tno: "desc"}
+    })
+
+    console.log(data)
+
     return (
         <div>
-            <div className={'text-4xl'}>TodoListPage</div>
+            <div className={'text-4xl'}>Todo List Page</div>
         </div>
     )
+
 }
 
 export default TodoListPage
